6f782134695349d7cb4718e38ae89314631bffac
Weed4221 <abhaycpwd9411@gmail.com>
2024-11-19 10:00:28 +0530

    Update Stock.java for lombok
    reduces boilers

--- src/main/java/com/stocks/entity/Stock.java
+++ src/main/java/com/stocks/entity/Stock.java
@@ -10,7 +10,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "stock")
 public class Stock {
@@ -50,59 +58,59 @@ public class Stock {
 
 	// Getters and setters
 
-	public Long getId() {
-		return id;
-	}
+	// public Long getId() {
+	// 	return id;
+	// }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+	// public void setId(Long id) {
+	// 	this.id = id;
+	// }
 
-	public String getTicker() {
-		return ticker;
-	}
+	// public String getTicker() {
+	// 	return ticker;
+	// }
 
-	public void setTicker(String ticker) {
-		this.ticker = ticker;
-	}
+	// public void setTicker(String ticker) {
+	// 	this.ticker = ticker;
+	// }
 
-	public Date getDate() {
-		return date;
-	}
+	// public Date getDate() {
+	// 	return date;
+	// }
 
-	public void setDate(Date date) {
-		this.date = date;
-	}
+	// public void setDate(Date date) {
+	// 	this.date = date;
+	// }
 
-	public BigDecimal getOpen() {
-		return open;
-	}
+	// public BigDecimal getOpen() {
+	// 	return open;
+	// }
 
-	public void setOpen(BigDecimal open) {
-		this.open = open;
-	}
+	// public void setOpen(BigDecimal open) {
+	// 	this.open = open;
+	// }
 
-	public BigDecimal getHigh() {
-		return high;
-	}
+	// public BigDecimal getHigh() {
+	// 	return high;
+	// }
 
-	public void setHigh(BigDecimal high) {
-		this.high = high;
-	}
+	// public void setHigh(BigDecimal high) {
+	// 	this.high = high;
+	// }
 
-	public BigDecimal getLow() {
-		return low;
-	}
+	// public BigDecimal getLow() {
+	// 	return low;
+	// }
 
-	public void setLow(BigDecimal low) {
-		this.low = low;
-	}
+	// public void setLow(BigDecimal low) {
+	// 	this.low = low;
+	// }
 
-	public BigDecimal getClose() {
-		return close;
-	}
+	// public BigDecimal getClose() {
+	// 	return close;
+	// }
 
-	public void setClose(BigDecimal close) {
-		this.close = close;
-	}
+	// public void setClose(BigDecimal close) {
+	// 	this.close = close;
+	// }
 }
